c1a05af726cf78399a157bb864519b2bb3ff0730
thanh <lmthanh203@gmail.com>
2023-09-23 14:38:12 +0700

    Thanh

--- app/src/main/java/com/example/doanphanmem/MainActivity.java
+++ app/src/main/java/com/example/doanphanmem/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+/*
+* Thanh*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
